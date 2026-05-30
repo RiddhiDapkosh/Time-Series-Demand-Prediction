46dd0bd1efe8e2844092f7c7927a566d8a41d723
Riddhi Dapkosh <riddhidapkosh@gmail.com>
2026-05-30 19:04:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -137,12 +137,12 @@ if predict_btn:
         st.success(f"📦 Predicted Demand: **{int(prediction)} units**")
 
             # Insight messages
-            if prediction < 10:
-                st.warning("⚠️ Low Demand Expected")
-            elif prediction < 30:
-                st.info("📊 Moderate Demand Expected")
-            else:
-                st.success("🔥 High Demand Expected")
+        if prediction < 10:
+            st.warning("⚠️ Low Demand Expected")
+        elif prediction < 30:
+            st.info("📊 Moderate Demand Expected")
+        else:
+            st.success("🔥 High Demand Expected")
 
         except Exception as e:
             st.error("Prediction failed ❌")
